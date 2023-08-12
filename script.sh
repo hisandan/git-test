05d04fc5ec9796b5829c9a6a09eded7fe311ba32
Daniel Santiago Sandoval Higuera <95585487+hisandan@users.noreply.github.com>
2023-08-12 00:22:15 +0000

    going to do test

--- script.sh
+++ script.sh
@@ -14,20 +14,45 @@ do
         "Enviar todos los cambios a repositorio personal")
             read -p "Escribe un mensaje corto descriptivo de los cambios: " commit_msg 
             git add .
-            git commit -m $commit_msg
+            git commit -m "- $commit_msg"
+            git fetch
             git rebase
-            add -> commit -> fetch -> rebase -> push 
-
+            git push
             echo "Datos enviados!"
             ;;
+
         "Enviar todos los  cambios a repositorio colaborativo")
-            echo "you chose choice 2"
+        add -> commit -> fetch -> rebase -> push -> pull request
+            read -p "Cual es el número de 'Issue' asociado a este envío?: " commit_msg 
+            git add .
+            git commit -m "- $commit_msg"
+            git fetch
+            git rebase
+            git push
+            git pull request????
+            echo "Datos enviados!"
             ;;
         "Sincronizar / Sobreescribir con repo personal (Elimina tus cambios en el editor)")
-            echo "you chose choice $REPLY which is $opt"
+            # git add .
+            # git commit -m "backup antes de actualizar forzadamente con info del repositorio personal"
+            # git push --force
+            git fetch upstream
+            git reset --hard upstream/main
+            git add .
+            git commit -m "Estado actual Repositorio Colaborativo"
+            git push --force
+            echo "Datos Actualizados!"
             ;;
         "Sincronizar / Sobreescribir con repositorio colaborativo (Elimina tus cambios en editor)")
-            echo "you chose choice $REPLY which is $opt"
+            git add .
+            git commit -m "backup antes de actualizar forzadamente con info del repositorio colaborativo"
+            git push --force
+            git fetch upstream
+            git reset --hard upstream/main
+            git add .
+            git commit -m "Estado actual Repositorio Colaborativo"
+            git push --force
+            echo "Datos Actualizados!"
             ;;
         "Salir")
             break
